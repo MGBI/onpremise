e0186da4692d386a432588a5145ac48bd67768a4
Mateusz (@pingwin) <grabekm90@gmail.com>
2019-11-21 12:14:59 +0100

    Add script for loading Rancher secrets in the docker image

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,8 @@
 ARG SENTRY_IMAGE
 FROM ${SENTRY_IMAGE}-onbuild
+
+# Script for loading sensitive data (secrets)
+COPY docker/file-env.sh /usr/local/src/
+COPY docker/load-secrets.sh /usr/local/bin/load-secrets
+# Loading available secret environment variables on shell start
+RUN echo 'source load-secrets' >> /etc/bash.bashrc
---
+++ docker/load-secrets.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+# Load secrets passed as the script parameters
+source /usr/local/src/file-env.sh
+
+SECRETS_DIR=/run/secrets
+
+vars_names=( "$@" )
+
+if [ -z "$1" ]; then
+	if [ -d $SECRETS_DIR ]; then
+		secrets_count=$(ls -A $SECRETS_DIR | wc -l)
+		if [ $secrets_count -gt 0 ]; then
+			echo "Found $secrets_count available secrets."
+			vars_names=( `ls -A $SECRETS_DIR | tr '[a-z]-' '[A-Z]_'` )
+		else
+			echo "No available secrets."
+		fi
+	else
+		echo "Secrets directory does not exist."
+	fi
+fi
+
+for var_name in "${vars_names[@]}"
+do
+	echo "Loading $var_name secret..."
+	file_env "$var_name"
+done
